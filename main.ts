1cb7943ee8a87792d4cfd8e449d07ff965802648
Beedlebub <beedlebub@gmail.com>
2020-09-02 13:50:58 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    pins.servoWritePin(AnalogPin.P8, buttonA_angle)
+})
+/**
+ * Servo seems to work best on 5v. 
+ * 
+ * Power servo directly from 5v.
+ * 
+ * Share same ground between add'l power and Micro:bit, but do NOT connect voltage together.
+ */
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    pins.servoWritePin(AnalogPin.P8, bothBottons_angle)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    pins.servoWritePin(AnalogPin.P8, buttonB_angle)
+})
+let bothBottons_angle = 0
+let buttonB_angle = 0
+let buttonA_angle = 0
+let minAngle = 20
+let maxAngle = 160
+buttonA_angle = 30
+buttonB_angle = 150
+bothBottons_angle = 90
+pins.servoWritePin(AnalogPin.P8, bothBottons_angle)
